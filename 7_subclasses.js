ed1bbe5ff6ff9ff9b20575dd56304b046dc7939c
Pavan Kumar <surisettypavankumar999@gmail.com>
2020-06-20 01:06:43 +0530

    Initial commit

---
+++ 7_subclasses.js
@@ -0,0 +1,24 @@
+class Book {
+    construction(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    }
+ getSummary() {
+     return `${this.title} was written by ${this.author} in ${this.Year}`
+ }
+}
+
+//Magazine Subclass
+class Magazine extends Book {
+    constructor(title, author, year, month) {
+        super(title, author, year);
+        this.month = month;
+    }
+}
+
+// Instantiate Magazine
+const mag1 = new Magazine('mag One', 'John Doe', '2018', 'Jan');
+const mag2 = new Magazine('mag Two', 'Jane Doe', '2016', 'June');
+
+console.log(mag2);
